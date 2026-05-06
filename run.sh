45eb44bfcc452629af0f86bd3d27f8ae2ba87198
darellanodev <darellano.aliaga@gmail.com>
2026-05-06 21:44:53 +0200

    feat: Add option for out-of-office directory in run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ echo "Select the directory pair:"
 echo "  1) php-design-patterns"
 echo "  2) tiny-loot-quest"
 echo "  3) portfolio-zola"
-read -p "Option [1-3]: " PAIR
+echo "  4) out-of-office"
+read -p "Option [1-4]: " PAIR
 
 # Main program
 case "$PAIR" in
@@ -22,8 +23,12 @@ case "$PAIR" in
     ORIGIN='D:/Descargas/portfolio-zola'
     DESTINY='D:/projects/portfolio/portfolio-zola'
     ;;
+  4)
+    ORIGIN='D:/Descargas/out-of-office'
+    DESTINY='D:/projects/games/out-of-office'
+    ;;
   *)
-    echo "Error: PAIR must be 1 or 2"
+    echo "Error: Incorrect option"
     exit 1
     ;;
 esac
