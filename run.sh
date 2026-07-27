bf2f3defca56d0898ead6edb72d7ac9ad5db4955
darellanodev <darellano.aliaga@gmail.com>
2026-07-27 22:06:38 +0200

    fix: Add dcss-help project in run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,18 @@ set -e
 
 # Ask the user
 echo "Select the directory pair:"
-echo "  1) php-design-patterns"
+echo "  1) pattern-lab-php"
 echo "  2) tiny-loot-quest"
 echo "  3) portfolio-zola"
 echo "  4) out-of-office"
-read -p "Option [1-4]: " PAIR
+echo "  5) dcss"
+read -p "Option [1-5]: " PAIR
 
 # Main program
 case "$PAIR" in
   1)
-    ORIGIN='D:/Descargas/php-design-patterns'
-    DESTINY='D:/xampp/htdocs/php-projects/php-design-patterns'
+    ORIGIN='D:/Descargas/pattern-lab-php'
+    DESTINY='D:/xampp/htdocs/php-projects/pattern-lab-php'
     ;;
   2)
     ORIGIN='D:/Descargas/tiny-loot-quest'
@@ -26,6 +27,10 @@ case "$PAIR" in
   4)
     ORIGIN='D:/Descargas/out-of-office'
     DESTINY='D:/projects/games/out-of-office'
+    ;;
+  5)
+    ORIGIN='D:/Descargas/dcss-help'
+    DESTINY='D:/projects/tools/dcss-help'
     ;;
   *)
     echo "Error: Incorrect option"
